22fa1f62f873740a0b7320036d12d520268ccfe6
thisisalim <83979489+thisisalim@users.noreply.github.com>
2025-08-14 17:27:06 +0400

    Delete vul2.py

--- vul2.py
+++
@@ -1,5 +0,0 @@
-# test_yaml_vuln.py
-import yaml
-
-vulnerable_data = "!!python/object/apply:os.system ['echo Vulnerable to YAML RCE']"
-yaml.load(vulnerable_data, Loader=yaml.Loader)  # Insecure usage (CVE-2017-18342)
